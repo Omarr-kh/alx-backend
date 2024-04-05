1cb256883fc4daced86d34b4d77b4ce9b74941fd
Omarr-kh <omar1752003@gmail.com>
2024-04-05 20:35:37 +0000

    LIFO cache class

---
+++ 0x01-caching/2-lifo_cache.py
@@ -0,0 +1,29 @@
+#!/usr/bin/env python3
+"""
+Deletion-resilient hypermedia pagination
+"""
+from base_caching import BaseCaching
+from collections import OrderedDict
+
+
+class LIFOCache(BaseCaching):
+    """ LIFOCache class """
+
+    def __init__(self):
+        """ init method """
+        super().__init__()
+        self.cache_data = OrderedDict()
+
+    def put(self, key, item):
+        """ add key/item pair to the cache dict """
+        if key and item:
+            if key not in self.cache_data:
+                if len(self.cache_data) + 1 > self.MAX_ITEMS:
+                    removed_key, _ = self.cache_data.popitem(True)
+                    print(f"DISCARD: {removed_key}")
+            self.cache_data[key] = item
+            self.cache_data.move_to_end(key, last=True)
+
+    def get(self, key):
+        """ Get an item from cache dict by key """
+        return self.cache_data.get(key)
