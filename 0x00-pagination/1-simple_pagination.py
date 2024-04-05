2aade3754c633fa9f4c186cc9fdf287d4fec0928
Omarr-kh <omar1752003@gmail.com>
2024-04-05 15:45:53 +0000

    removed trailing whitespace

--- 0x00-pagination/1-simple_pagination.py
+++ 0x00-pagination/1-simple_pagination.py
@@ -8,7 +8,7 @@ from typing import Tuple, List
 def index_range(page: int, page_size: int) -> Tuple:
     """ Returns  a tuple of size two containing a start index and
     an end index corresponding to the range of indexes to return
-    in a list for those particular pagination parameters. 
+    in a list for those particular pagination parameters.
     """
 
     start_index = (page - 1) * page_size
