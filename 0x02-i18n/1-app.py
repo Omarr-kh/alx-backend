f90285473d1c5bbd2848055e2c933b8a26c43a36
Omarr-kh <omar1752003@gmail.com>
2024-04-10 00:29:45 +0000

    task 1

---
+++ 0x02-i18n/1-app.py
@@ -0,0 +1,27 @@
+#!/usr/bin/env python3
+""" Flask Application """
+from flask import Flask, render_template
+from flask_babel import Babel
+
+app = Flask(__name__)
+
+
+class Config:
+    """ Configuration class """
+    LANGUAGES = ["en", "fr"]
+    BABEL_DEFAULT_LOCALE = "en"
+    BABEL_DEFAULT_TIMEZONE = "UTC"
+
+
+app.config.from_object(Config)
+babel = Babel(app)
+
+
+@app.route('/', strict_slashes=False)
+def home():
+    """ home template """
+    return render_template("1-index.html")
+
+
+if __name__ == "__main__":
+    app.run()
