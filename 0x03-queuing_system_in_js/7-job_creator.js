af60d00f5275515ae816e08e0b35b96b13146594
Omar <omar1752003@gmail.com>
2024-05-07 17:43:54 +0000

    job creation and processing

--- 0x03-queuing_system_in_js/7-job_creator.js
+++ 0x03-queuing_system_in_js/7-job_creator.js
@@ -49,14 +49,14 @@ const jobs = [
 
 const queue = kue.createQueue();
 
-for (const jobData in jobs) {
+for (const jobData of jobs) {
   const job = queue.create('push_notification_code_2', jobData).save((err) => {
     if (!err) console.log(`Notification job created: ${job.id}`);
   });
 
   job
     .on('complete', () => console.log(`Notification job ${job.id} completed`))
-    .on('failure', (failErr) =>
+    .on('failed', (failErr) =>
       console.log(`Notification job ${job.id} failed: ${failErr}`)
     )
     .on('progress', (progress, data) =>
